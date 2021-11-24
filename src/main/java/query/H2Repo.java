ae9144b8e49d74dfa70ff4643b9e8e41987de6f5
milad.bourhani <milad.bourhani@accenture.com>
2021-11-24 09:56:26 +0100

    tidy up

--- src/main/java/query/H2Repo.java
+++ src/main/java/query/H2Repo.java
@@ -8,46 +8,40 @@ import java.util.ArrayList;
 
 import static java.util.stream.Collectors.joining;
 
-public class H2Repo implements Repository
-{
+public class H2Repo implements Repository {
     @Override
-    public Try<List<List<Entity>>> select(Query q)
-    {
+    public Try<List<List<Entity>>> select(Query q) {
         return Try.of(() -> queryEntities(q));
     }
 
     @Override
-    public Try<List<List<Object>>> select(Select s)
-    {
+    public Try<List<List<Object>>> select(Select s) {
         return select(s.from()).map(es -> toObjects(s, es));
     }
 
     private List<List<Object>> toObjects(
             Select sq,
             List<List<Entity>> es
-    )
-    {
+    ) {
         return es.map(e -> project(e, sq.select()));
     }
 
     private List<Object> project(
             List<Entity> es,
             List<Term> select
-    )
-    {
+    ) {
         return select.map(t ->
-                switch (t) {
-                    case AttrTerm at -> es.flatMap(Entity::attrs)
-                            .filter(a -> a.attr().equals(at.attr()))
-                            .map(this::attrValue)
-                            .get();
-                    case Value v -> v.value();
-                    case Null ignored -> null;
-                });
+                                  switch (t) {
+                                      case AttrTerm at -> es.flatMap(Entity::attrs)
+                                              .filter(a -> a.attr().equals(at.attr()))
+                                              .map(this::attrValue)
+                                              .get();
+                                      case Value v -> v.value();
+                                      case Null ignored -> null;
+                                  });
     }
 
-    private Object attrValue(AttrValue av)
-    {
+    private Object attrValue(AttrValue av) {
         return switch (av) {
             case StrAttrValue sv -> sv.value();
             case IntAttrValue iv -> iv.value();
@@ -55,49 +49,12 @@ public class H2Repo implements Repository
     }
 
     private List<List<Entity>> queryEntities(Query q)
-    throws SQLException
-    {
-        String select =
-                "select " +
-                q.from().et().name() + ".id, " +
-                q.from().et().attrs()
-                        .map(attr -> q.from().et().name() + "." + attr.name())
-                        .collect(joining(", ")) +
-                (q.joins().isEmpty() ? "" : ", ") +
-                q.joins()
-                        .map(j -> j.from().et().name() + ".id, ")
-                        .collect(joining(", ")) +
-                q.joins()
-                        .flatMap(j -> j.from().et().attrs().map(
-                                attr -> j.from().et().name() + "." + attr.name()))
-                        .collect(joining(", "));
+    throws SQLException {
+        String sql = toSqlQuery(q);
 
-        String from =
-                "from " + q.from().et().name() +
-                " as " +
-                q.from().et().name().toLowerCase();
-
-        String joins = q.joins()
-                .map(j -> "join " + j.from().et().name() + " " +
-                          "on " + j.on()
-                                  .map(this::toSql)
-                                  .collect(joining(" and ")))
-                .collect(joining("\n"));
-
-        String where =
-                q.where().isEmpty()
-                        ? ""
-                        : ("where " + q.where()
-                        .map(this::toSql)
-                        .collect(joining(" and ")));
-
-        String sql = select + "\n" +
-                     from + "\n" +
-                     joins + "\n" +
-                     where;
-
-        try (Connection c = createConnection();
-             PreparedStatement selectStatement = c.prepareStatement(sql)
+        try (
+                Connection c = createConnection();
+                PreparedStatement selectStatement = c.prepareStatement(sql)
         ) {
             ResultSet rs = selectStatement.executeQuery();
             java.util.List<java.util.List<Entity>> rows = new ArrayList<>();
@@ -112,6 +69,58 @@ public class H2Repo implements Repository
             }
             return List.ofAll(rows).map(List::ofAll);
         }
+    }
+
+    private String toSqlQuery(Query q) {
+        String select = toSqlSelect(q);
+        String from = toSqlFrom(q.from());
+        String joins = toSqlJoins(q.joins());
+        String where = toSqlWhere(q.where());
+
+        return select + "\n" +
+               from + "\n" +
+               joins + "\n" +
+               where;
+    }
+
+    private String toSqlSelect(Query query) {
+        return
+                "select " +
+                query.from().et().name() + ".id, " +
+                query.from().et().attrs()
+                        .map(attr -> query.from().et().name() + "." + attr.name())
+                        .collect(joining(", ")) +
+                (query.joins().isEmpty() ? "" : ", ") +
+                query.joins()
+                        .map(j -> j.from().et().name() + ".id, ")
+                        .collect(joining(", ")) +
+                query.joins()
+                        .flatMap(j -> j.from().et().attrs().map(
+                                attr -> j.from().et().name() + "." + attr.name()))
+                        .collect(joining(", "));
+    }
+
+    private String toSqlFrom(From from) {
+        return "from " + from.et().name() +
+               " as " +
+               from.et().name().toLowerCase();
+    }
+
+    private String toSqlJoins(List<Join> joins) {
+        return joins
+                .map(j -> "join " + j.from().et().name() + " " +
+                          "on " + j.on()
+                                  .map(this::toSql)
+                                  .collect(joining(" and ")))
+                .collect(joining("\n"));
+    }
+
+    private String toSqlWhere(List<Predicate> w) {
+        return w.isEmpty()
+                ? ""
+                : ("where " + w
+                .map(this::toSql)
+                .collect(joining(" and ")));
     }
 
     private Entity readEntity(EntityType et, ResultSet rs)
@@ -134,26 +143,26 @@ public class H2Repo implements Repository
         return new Entity(et, id, List.ofAll(avs));
     }
 
-    public void init(EntityType... types) throws SQLException
-    {
+    public void init(EntityType... types) throws SQLException {
         for (EntityType type : types) {
-            try (Connection c = createConnection();
-                 Statement s = c.createStatement()) {
+            try (
+                    Connection c = createConnection();
+                    Statement s = c.createStatement()
+            ) {
                 s.execute("drop table if exists " + type.name());
                 String columnsDdl =
                         List.of("id varchar primary key")
-                        .appendAll(type.attrs().map(this::colDdl))
-                        .collect(joining(", "));
+                                .appendAll(type.attrs().map(this::colDdl))
+                                .collect(joining(", "));
 
                 String ddl = "create table " + type.name() +
-                        " (" + columnsDdl + ")";
+                             " (" + columnsDdl + ")";
                 s.execute(ddl);
             }
         }
     }
 
-    private String colDdl(Attr attr)
-    {
+    private String colDdl(Attr attr) {
         return attr.name() + switch (attr.type()) {
             case Str -> " varchar";
             case Int -> " int";
@@ -195,8 +204,7 @@ public class H2Repo implements Repository
         };
     }
 
-    public void addEntities(Entity... es) throws SQLException
-    {
+    public void addEntities(Entity... es) throws SQLException {
         try (Connection c = createConnection()) {
             for (Entity e : es) {
                 String dml = List.of("id")
@@ -204,17 +212,20 @@ public class H2Repo implements Repository
                         .collect(joining(", "));
 
                 int idx = 0;
-                try (PreparedStatement insert = c.prepareStatement(
-                        "insert into " + e.type().name() +
+                try (
+                        PreparedStatement insert = c.prepareStatement(
+                                "insert into " + e.type().name() +
                                 " (" + dml + ") values (" +
-                                "?" + ", ?".repeat(e.attrs().size()) + ")")
+                                "?" + ", ?" .repeat(e.attrs().size()) + ")")
                 ) {
                     insert.setString(++idx, e.id());
                     for (AttrValue a : e.attrs()) {
                         switch (a) {
-                            case StrAttrValue s -> insert.setString(++idx,
+                            case StrAttrValue s -> insert.setString(
+                                    ++idx,
                                     s.value());
-                            case IntAttrValue i -> insert.setInt(++idx,
+                            case IntAttrValue i -> insert.setInt(
+                                    ++idx,
                                     i.value());
                         }
                     }
@@ -225,8 +236,7 @@ public class H2Repo implements Repository
     }
 
     private static Connection createConnection()
-    throws SQLException
-    {
+    throws SQLException {
         return DriverManager.getConnection(
                 "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "");
     }
