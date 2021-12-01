015055bf8d8ce36a68298bce8c5408bfbac203df
Milad Bourhani <milad.bourhani@gmail.com>
2021-12-01 21:03:43 +0100

    tidy up

--- src/main/java/query/MongoRepo.java
+++ src/main/java/query/MongoRepo.java
@@ -79,11 +79,6 @@ public class MongoRepo
         MongoCollection<Document> coll =
                 db.getCollection(q.from().et().name());
 
-        Bson include = include(q.select()
-                .map(this::toFieldName)
-                .toJavaList());
-        Bson projection = fields(include, excludeId());
-
         java.util.List<Bson> pipeline = new ArrayList<>();
 
         if (!q.where().isEmpty())
@@ -95,6 +90,11 @@ public class MongoRepo
         if (!q.groupBy().isEmpty())
             pipeline.addAll(toGroupByDocs(q).toJavaList());
 
+        Bson include = include(q.select()
+                .map(this::toFieldName)
+                .toJavaList());
+        Bson projection = fields(include, excludeId());
+
         pipeline.add(project(projection));
 
         if (!q.orderBy().isEmpty())
@@ -103,7 +103,10 @@ public class MongoRepo
         AggregateIterable<Document> find = coll.aggregate(pipeline);
 
         MongoIterable<List<Object>> map =
-                find.map(d -> q.select().map(s -> d.get(toFieldName(s))));
+                find.map(d -> q.select()
+                        .map(this::toFieldName)
+                        .map(d::get));
+
         ArrayList<List<Object>> l = new ArrayList<>();
         map.into(l);
         return List.ofAll(l);
@@ -111,15 +114,18 @@ public class MongoRepo
 
     private String toFieldName(SelectTerm at) {
         return switch (at) {
-            case AttrSelectTerm ast -> ast.attr().name();
-            case Aggregation aggr -> aggr.at().name().toLowerCase() +
+            case AttrSelectTerm ast ->
+                    ast.attr().name();
+            case Aggregation a ->
+                    a.at().name().toLowerCase() +
                     "_" +
-                    aggr.t().attr().name();
+                    a.t().attr().name();
         };
     }
 
     private List<Bson> toJoinDocs(Query q) {
-        return q.joins().flatMap(join -> toJoinDoc(q.from(), join));
+        return q.joins()
+                .flatMap(join -> toJoinDoc(q.from(), join));
     }
 
     private List<Bson> toJoinDoc(
@@ -131,14 +137,16 @@ public class MongoRepo
 
         if (join.on().size() != 1
                 || !(join.on().get(0) instanceof BinOp on)
-                || !(((BinOp) join.on().get(0)).right() instanceof AttrClauseTerm foreign))
-            throw new UnsupportedOperationException();
+                || !(((BinOp) join.on().get(0)).right()
+                instanceof AttrClauseTerm attrTerm))
+            throw new UnsupportedOperationException(
+                    "only simple joins supported");
 
-        String localField = on.left().attr().name();
-        String foreignField = foreign.attr().name();
+        String local = on.left().attr().name();
+        String foreign = attrTerm.attr().name();
 
         return List.of(
-                lookup(joinedColl, localField, foreignField, joinName),
+                lookup(joinedColl, local, foreign, joinName),
                 match(ne(joinName, java.util.List.of())),
                 new Document(
                         "$addFields",
@@ -157,34 +165,29 @@ public class MongoRepo
                 .flatMap(gb -> q.select()
                         .filter(s -> s instanceof Aggregation)
                         .map(s -> (Aggregation) s)
-                        .map(m -> Tuple.of(m, gb)));
+                        .map(a -> Tuple.of(a, gb)));
         return groups
                 .flatMap(this::toGroupByDoc);
     }
 
     private List<Bson> toGroupByDoc(Tuple2<Aggregation, Attr> g) {
-        String aggregatedFieldName = g._1.t().attr().name();
-        String groupedByFieldName = g._2.name();
-        String aggrModeName = g._1.at().name().toLowerCase();
+        String field = g._1.t().attr().name();
+        String group = g._2.name();
+        String mode = g._1.at().name().toLowerCase();
 
-        BsonField aggrField = switch (g._1.at()) {
-            case SUM -> sum(aggrModeName + "_" + aggregatedFieldName,
-                    "$" + aggregatedFieldName);
-            case MAX -> max(aggrModeName + "_" + aggregatedFieldName,
-                    "$" + aggregatedFieldName);
-            case MIN -> min(aggrModeName + "_" + aggregatedFieldName,
-                    "$" + aggregatedFieldName);
-            case AVG -> avg(aggrModeName + "_" + aggregatedFieldName,
-                    "$" + aggregatedFieldName);
-            case COUNT -> sum(aggrModeName + "_" + aggregatedFieldName, 1);
+        BsonField aggregationField = switch (g._1.at()) {
+            case SUM -> sum(mode + "_" + field, "$" + field);
+            case MAX -> max(mode + "_" + field, "$" + field);
+            case MIN -> min(mode + "_" + field, "$" + field);
+            case AVG -> avg(mode + "_" + field, "$" + field);
+            case COUNT -> sum(mode + "_" + field, 1);
         };
 
         return List.of(
-                group("$" + groupedByFieldName,
-                        aggrField),
+                group("$" + group, aggregationField),
                 project(fields(
-                        include(aggrModeName + "_" + aggregatedFieldName),
-                        new BsonDocument(groupedByFieldName, new BsonString("$_id")),
+                        include(mode + "_" + field),
+                        new BsonDocument(group, new BsonString("$_id")),
                         excludeId()))
         );
     }
@@ -210,9 +213,9 @@ public class MongoRepo
                 case GT -> gt(bo.left().attr().name(), toTermDoc(bo.right()));
                 case LIKE -> throw new UnsupportedOperationException("like");
             };
-            case And and -> and(toFilterDoc(and.left()), toFilterDoc(and.right()));
-            case Or or -> or(toFilterDoc(or.left()), toFilterDoc(or.right()));
-            case Not not -> not(toFilterDoc(not.predicate()));
+            case And a -> and(toFilterDoc(a.left()), toFilterDoc(a.right()));
+            case Or o -> or(toFilterDoc(o.left()), toFilterDoc(o.right()));
+            case Not n -> not(toFilterDoc(n.predicate()));
         };
     }
 
